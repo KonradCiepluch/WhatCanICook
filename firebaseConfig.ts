4dc417a4dfaa3596934735a87e555829ee3b4761
Konrad Ciepluch <konrad190008@wp.pl>
2024-11-21 23:47:20 +0100

    Chore: removed envs

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyCWtR1uESHyhTFYL5f2nNJ17iT3Rjg6QbI',
-  authDomain: 'whatcanicook-d4cbd.firebaseapp.com',
-  projectId: 'whatcanicook-d4cbd',
-  storageBucket: 'whatcanicook-d4cbd.appspot.com',
-  messagingSenderId: '410568094717',
-  appId: '1:410568094717:web:7f32f6c5ce214373e41f94',
-  measurementId: 'G-LFBS2W43QB',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
