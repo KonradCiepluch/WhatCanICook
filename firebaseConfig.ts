1a8c9484de65c5adbe71263d0c8ec10165b59478
Konrad Ciepluch <konrad190008@wp.pl>
2024-11-21 01:06:34 +0100

    Chore: added temporary hardcoded api key

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.API_KEY || 'AIzaSyCWtR1uESHyhTFYL5f2nNJ17iT3Rjg6QbI',
+  authDomain: process.env.AUTH_DOMAIN!,
+  projectId: process.env.PROJECT_ID!,
+  storageBucket: process.env.STORAGE_BUCKET!,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID!,
+  appId: process.env.APP_ID!,
+  measurementId: process.env.MEASUREMENT_ID!,
 };
 
 export default firebaseConfig;
--- firebaseInit/firebase.ts
+++ firebaseInit/firebase.ts
@@ -1,36 +1,16 @@
 import { initializeApp } from 'firebase/app';
 
-import { getAnalytics } from 'firebase/analytics';
-
 import { getFirestore, collection, doc } from 'firebase/firestore';
 
 import { getAuth, FacebookAuthProvider, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
-import firebaseConfig from 'firebaseConfig';
+import firebaseConfig from '../firebaseConfig';
 
 import { getStorage } from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
-// init storage
-
 const storage = getStorage(app);
-
-// const analytics = getAnalytics(app);
-
-// init services
-
-// init firestore
 
 const db = getFirestore(app);
 
